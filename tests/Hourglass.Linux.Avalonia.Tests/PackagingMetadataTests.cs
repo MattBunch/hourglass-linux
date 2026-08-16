8869cffa9e833d46bf41ab2ba7ececb4c728c408
matt <mattbunch@protonmail.com>
2026-08-16 20:52:27 +1200

    Prepare 0.2.0-beta.1 release

--- tests/Hourglass.Linux.Avalonia.Tests/PackagingMetadataTests.cs
+++ tests/Hourglass.Linux.Avalonia.Tests/PackagingMetadataTests.cs
@@ -41,9 +41,9 @@ public sealed class PackagingMetadataTests
             "https://github.com/MattBunch/hourglass-linux/issues",
             component.Elements("url").Single(element => element.Attribute("type")?.Value == "bugtracker").Value);
 
-        XElement release = Assert.Single(component.Element("releases")?.Elements("release") ?? []);
+        XElement release = Assert.IsType<XElement>(component.Element("releases")?.Elements("release").FirstOrDefault());
         Assert.Equal(projectVersion, release.Attribute("version")?.Value);
-        Assert.Equal("2026-07-18", release.Attribute("date")?.Value);
+        Assert.Equal("2026-08-16", release.Attribute("date")?.Value);
     }
 
     [Fact]
