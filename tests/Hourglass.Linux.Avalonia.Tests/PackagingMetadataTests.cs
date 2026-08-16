6763d3f3394f5d0392bb2d6fac29188dc579213a
matt <mattbunch@protonmail.com>
2026-08-16 21:27:04 +1200

    Mark beta AppStream release as development

--- tests/Hourglass.Linux.Avalonia.Tests/PackagingMetadataTests.cs
+++ tests/Hourglass.Linux.Avalonia.Tests/PackagingMetadataTests.cs
@@ -44,6 +44,7 @@ public sealed class PackagingMetadataTests
         XElement release = Assert.IsType<XElement>(component.Element("releases")?.Elements("release").FirstOrDefault());
         Assert.Equal(projectVersion, release.Attribute("version")?.Value);
         Assert.Equal("2026-08-16", release.Attribute("date")?.Value);
+        Assert.Equal("development", release.Attribute("type")?.Value);
     }
 
     [Fact]
